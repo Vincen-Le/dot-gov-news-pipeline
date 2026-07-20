46897a3f12ad999139b00706bf6455f88fb0376a
Vincent Le <vincen_le@berkeley.edu>
2026-07-20 10:30:09 -0400

    chore(images): complete agency thumbnail backfill

--- apps/image_and_synthesis_gen/src/reusable/completed.ts
+++ apps/image_and_synthesis_gen/src/reusable/completed.ts
@@ -131,4 +131,96 @@ export const COMPLETED_REUSABLE_IMAGES: readonly CompletedReusableImage[] = [
     generatedFileName: "exec-de7e0192-693f-4c8b-8456-a03be69588d3.png",
     key: "ftc",
   },
+  {
+    generatedFileName: "exec-4387e730-0b2c-4055-b577-e34285367608.png",
+    key: "osha",
+  },
+  {
+    generatedFileName: "exec-7b220deb-35f6-47f8-b05a-610ded5f37fc.png",
+    key: "sec",
+  },
+  {
+    generatedFileName: "exec-83bff146-615d-4f0d-b42a-f8617d49cbc0.png",
+    key: "sec-enforcement",
+  },
+  {
+    generatedFileName: "exec-684559f6-38f7-4e6d-8114-2df2dc8b8369.png",
+    key: "inciweb",
+  },
+  {
+    generatedFileName: "exec-80a98087-776a-4c7b-bbdd-7193e927a788.png",
+    key: "irs",
+  },
+  {
+    generatedFileName: "exec-ee9f5159-c3f8-42a5-891f-0ddc52b1187e.png",
+    key: "nasa",
+  },
+  {
+    generatedFileName: "exec-03b24d90-2f03-41e3-addc-19b2dd5342d6.png",
+    key: "ssa",
+  },
+  {
+    generatedFileName: "exec-4b026110-a69c-4917-9444-39d28dea509e.png",
+    key: "state",
+  },
+  {
+    generatedFileName: "exec-a97491da-6f5e-4a0c-90c1-b1c120eca947.png",
+    key: "texas-gov",
+  },
+  {
+    generatedFileName: "exec-664aec2a-1999-4b2f-816c-9fc5b85fb09f.png",
+    key: "ncbi",
+  },
+  {
+    generatedFileName: "exec-eb4b99a7-145d-40fd-81ef-d81e9caabf33.png",
+    key: "noaa",
+  },
+  {
+    generatedFileName: "exec-0e990bcb-8eac-40a6-ab6a-b02143d21188.png",
+    key: "bls",
+  },
+  {
+    generatedFileName: "exec-9880c625-5dce-42bc-bc09-8f21a5915615.png",
+    key: "cdc",
+  },
+  {
+    generatedFileName: "exec-fc03594b-ef15-4596-8922-42bd0f320d2b.png",
+    key: "cftc",
+  },
+  {
+    generatedFileName: "exec-6c8ff7b2-a7c3-4b02-971d-47d3c9bd2590.png",
+    key: "cisa",
+  },
+  {
+    generatedFileName: "exec-ed1771ff-b567-4126-b62b-85d27599b974.png",
+    key: "csb",
+  },
+  {
+    generatedFileName: "exec-a8993fba-a2c6-4c0d-9ca8-1300379f507e.png",
+    key: "doj",
+  },
+  {
+    generatedFileName: "exec-3a966e18-5710-4dfc-85d4-f469f0e5864e.png",
+    key: "treasury",
+  },
+  {
+    generatedFileName: "exec-9afdcf77-e318-4a33-9abf-72546cf666dd.png",
+    key: "uscis",
+  },
+  {
+    generatedFileName: "exec-f690b152-6c28-4ccc-b927-18cbfd786a24.png",
+    key: "usda",
+  },
+  {
+    generatedFileName: "exec-e4fe7350-4bc3-4a32-ba8b-c6e1770294df.png",
+    key: "usgs",
+  },
+  {
+    generatedFileName: "exec-38dee9da-ee15-4993-b6f3-b110317b57f2.png",
+    key: "usps",
+  },
+  {
+    generatedFileName: "exec-aa095b5a-7ab4-4172-9cf0-5bc5e54b1a91.png",
+    key: "va",
+  },
 ];
